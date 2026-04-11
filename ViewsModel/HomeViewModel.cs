c726953f479e3a6b1ae49e31714d8e25423628b1
MahdiSHams <cd /path/to/your/repository>
2026-04-11 12:25:50 +0330

    conect to backend agent and about and service

--- ViewsModel/HomeViewModel.cs
+++ ViewsModel/HomeViewModel.cs
@@ -1,9 +1,47 @@
 using System.ComponentModel.DataAnnotations; // برای Data Annotations
-
-// مدل طبقه
 public class HomeViewModel
 {
     public int advertisementsCount { get; set; }
-    public List<Advertisement> advertisements{ get; set; }
-    public List<StaticData> staticDatas{ get; set; }
+    public List<Advertisement> advertisements { get; set; }
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class aboutViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class agentViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class blogViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class blogSingleViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class contactViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class propertiesViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class propertiesSingleViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
+}
+
+public class servicesViewModel
+{
+    public List<StaticData> staticDatas { get; set; }
 }
